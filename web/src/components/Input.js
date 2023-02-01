09f4af61c84375fdfa63b6978e41f3445a5353b0
Tony Park <tonynotmorty@gmail.com>
2023-02-01 22:59:30 +0900

    Update Input.js

--- web/src/components/Input.js
+++ web/src/components/Input.js
@@ -1,11 +1,27 @@
 import React, { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
-export default function Input({ className, children, limit, ...props }) {
+export default function Input({
+  className,
+  onChange,
+  children,
+  limit,
+  ...props
+}) {
   return (
-    <div className="flex w-full flex-col text-start text-secondary">
+    <div className="relative flex w-full flex-col text-start text-secondary">
       <input
         {...props}
+        onChange={(e) => {
+          let value = e.target.value;
+          if (limit) {
+            if (value.length <= limit) {
+              onChange(value);
+            }
+          } else {
+            onChange(value);
+          }
+        }}
         className={twMerge(
           "h-[54px] w-full min-w-[260px] border border-input-border bg-bg px-4 text-lg text-primary outline-none transition-all placeholder:text-secondary focus:outline-none",
           className

71b383cb56adb806bd973b610fc414af19727492
Tony Park <tonynotmorty@gmail.com>
2023-02-02 14:51:24 +0900

    CreategetHourMinuteSeconds function

--- web/src/utils/numbers.js
+++ web/src/utils/numbers.js
@@ -14,3 +14,15 @@ export function formatHours(time) {
     .toString()
     .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 }
+
+export function getHourMinuteSeconds(seconds) {
+  const h = Math.floor(seconds / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = seconds % 60;
+
+  return {
+    h,
+    m,
+    s,
+  };
+}

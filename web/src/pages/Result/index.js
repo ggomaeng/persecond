4a4918faa3fbb57b36bfeb91148733f6d96418ad
Tony Park <tonynotmorty@gmail.com>
2023-02-02 18:18:57 +0900

    Result Index page clean up

--- web/src/pages/Result/index.js
+++ web/src/pages/Result/index.js
@@ -4,14 +4,9 @@ import Host from "./Host";
 import Expert from "./Expert";
 import Loading from "components/Loading";
 import { Link } from "react-router-dom";
-import { abbreviateAddress } from "utils/address";
 
 export default function HostResult() {
   const [isLoading, setIsLoading] = useState(true);
-  const dummy = {
-    title: "Aptos' Mo Shaikh on the Move Moment",
-    duration: 16,
-  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -28,7 +23,7 @@ export default function HostResult() {
           </Link>
           <div className="mt-10 flex w-[480px] flex-col items-center border border-[#4a4a4a] bg-modal-bg py-15 px-10 text-center">
             <img className="w-[80px]" src="/assets/logo-single@2x.png" alt="" />
-            {false ? <Host /> : <Expert />}
+            {true ? <Host /> : <Expert />}
           </div>
         </>
       ) : (

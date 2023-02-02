8bce94f7ac656a11a6f89d363a9af1210a086ca8
Tony Park <tonynotmorty@gmail.com>
2023-02-02 17:56:40 +0900

    Update index.js

--- web/src/pages/Join/index.js
+++ web/src/pages/Join/index.js
@@ -4,6 +4,7 @@ import Button from "components/Button";
 import Header from "components/Header";
 import { formatHours } from "utils/numbers";
 import { fixDecimalPlaces } from "utils/numbers";
+import { abbreviateAddress } from "utils/address";
 
 export default function Join() {
   const dummy = {
@@ -17,23 +18,29 @@ export default function Join() {
   return (
     <div className="flex h-screen flex-col justify-center padded-horizontal">
       <Header />
-      <div className="text-3xl text-primary">{dummy.title}</div>
-      <div className="mt-1 text-lg text-secondary">{dummy.description}</div>
+      <h1 className="text-3xl text-primary">Join the perSecond meeting</h1>
+      <h2 className="mt-1 text-lg text-secondary">
+        You have received a request for a perSecond meeting. You will be paid
+        according to the rate set by the requester for the exact amount of time
+        spent in the meeting.
+      </h2>
       <hr className="mt-5 mb-10 border-input-border" />
       <div className="flex flex-col tablet:flex-row">
         <div className="flex flex-col gap-5.5 tablet:w-1/2">
           <img src="/assets/aptos-logo@2x.png" alt="" />
-          <div className="border-1 w-full border border-primary bg-modal-bg p-5 text-secondary">
-            <div className="text-2xl text-primary">Session information</div>
+          <div className="border-1 w-full border border-primary bg-modal-bg p-5 text-lg text-secondary">
+            <div className="text-2xl text-primary">Meeting information</div>
             <div className="mt-2">
               Price:
-              <span className="font-bold text-primary"> ${dummy.price} </span>/
-              sec
+              <span className="font-bold text-primary"> {dummy.price} APT</span>
+              <span> / sec ({dummy.price} APT / hour)</span>
             </div>
-            <div>Current: {3} audiences</div>
-            <div>Total: {17} audiences</div>
+            <div>Max duration: 2 hours</div>
             <div>
-              Duration: {`15:04:31 `} / {formatHours(dummy.duration)}
+              Requestor:{" "}
+              {abbreviateAddress(
+                "0xe5a07359699b5457ab8db5f285765e7e115061b9b00de4ebca6a9d943e086d1e"
+              )}
             </div>
           </div>
         </div>
@@ -49,39 +56,10 @@ export default function Join() {
               Connect wallet
             </Button>
           </StepBase>
-          <StepBase count={2} title="Validation check">
-            <div className="flex items-start gap-2">
-              <div className="flex h-7 items-center">
-                <img src="/assets/check-green.svg" className="w-5.5" alt="" />
-              </div>
-              <div className="flex flex-col">
-                <div>Sufficent balance to make a deposit to join</div>
-                <div>
-                  My balance:
-                  <span className="font-bold text-primary">
-                    {" "}
-                    {fixDecimalPlaces(152, 2)} APT
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div className="mt-2 flex items-start gap-2">
-              <div className="flex h-7 items-center">
-                <img src="/assets/check-grey.svg" className="w-5.5" alt="" />
-              </div>
-              <div className="flex flex-col">
-                <div>Need to approve USDC</div>
-                <Button className="mt-2.5 flex items-center justify-center font-semibold">
-                  Approve APTOS
-                </Button>
-              </div>
-            </div>
-          </StepBase>
-          <StepBase count={3} title="Join the session">
+          <StepBase count={2} title="Join the meeting">
             <div>
-              <span className="font-bold text-primary">50.23 USDC</span> held as
-              full-duration deposit to join session. Any remaining balance
-              refunded based on actual session time.
+              By making this transaction, you are agreeing to participate in the
+              meeting and receive the time-based payment.
             </div>
             <Button className="mt-5 flex items-center justify-center font-semibold">
               Proceed to join

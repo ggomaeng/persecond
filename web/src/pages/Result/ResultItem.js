a51df9b6757c7b1bab97876b77a7981983805aa3
Tony Park <tonynotmorty@gmail.com>
2023-02-02 18:11:49 +0900

    Update ResultItem.js

--- web/src/pages/Result/ResultItem.js
+++ web/src/pages/Result/ResultItem.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function ResultItem({ title, children }) {
   return (
-    <div className="flex w-full flex-col items-start border border-secondary p-3 text-secondary">
+    <div className="flex w-full flex-col items-start border border-option-border p-3 text-secondary">
       <div className="mb-2 text-lg text-secondary">{title}</div>
-      <div className="self-end">{children}</div>
+      <div className="self-end text-end">{children}</div>
     </div>
   );
 }
